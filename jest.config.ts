4495729f8bfe049217d590ac94bf95f615c59359
maxinth <tunde.j.adeleye@gmail.com>
2025-03-07 11:17:52 +0100

    cleanup for jest setup.

--- jest.config.ts
+++ jest.config.ts
@@ -1,10 +1,8 @@
 export default {
   preset: "ts-jest",
   testEnvironment: "jsdom",
-  setupFilesAfterEnv: ["<rootDir>/setupTests.ts"],
-  globals: {
-    "ts-jest": {
-      tsconfig: "<rootDir>/tsconfig.test.json",
-    },
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  transform: {
+    "^.+\\.tsx?$": ["ts-jest", { tsconfig: "<rootDir>/tsconfig.test.json" }],
   },
 };
